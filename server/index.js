8ba85ff340719639b728c3871f965337e3c513f1
Ruth Nnochiri <ruthnnochiri@gmail.com>
2018-06-29 14:19:22 +0100

    set up database
    - set up database
    - create users table
    - create rides table
    - create ride request table
    
    [Finishes #158697535]

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
+import Pool from 'pg';
 import router from './routes';
 
 const app = express();
 
+const connectionString = 'postgres://rffgwprh:dfbnXahzqPCnuaAkuxdPafGYWQMv9HlZ@stampy.db.elephantsql.com:5432/rffgwprh';
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const pool = new Pool({
+  connectionString,
+});
 
 app.use('/api/v1', router);
 
